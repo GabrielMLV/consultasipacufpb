c9d41681c84aff9ec1d8a4cb98f3b0d6741dabf2
Gabriel Matheus <bielmatheus20@gmail.com>
2020-08-08 18:21:04 -0300

    new layout and functions get auxilio by campus

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,15 @@
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow'); 
+    $('body').delay(350).css({'overflow': 'visible'});
+    //localStorage.setItem('nome','Jack Sparrow22');
+    const verifyLastSearch = localStorage.getItem('last_search');
+    if(verifyLastSearch !== null){
+        document.getElementById("lastSearch").innerHTML = verifyLastSearch;
+    }else{
+        document.getElementById("lastSearch").innerHTML = '<span style="font-size:13px;">Bem-vindo!</span>'
+    }
+})
 var loadSpinn = '<span class="uk-flex uk-flex-center" uk-spinner="ratio: 4.5"></span>';
 var obj_default = [
     {
@@ -42,9 +54,101 @@ var obj_default = [
     }
 ]
 
+var templateCountDown = `<div style="display: inline-flex;margin: 0px 0px 0px 0px !important;" class="uk-flex uk-flex-middle uk-flex-center uk-text-center uk-grid-small uk-child-width-auto uk-margin" uk-grid uk-countdown="date: {0}">
+<label style="padding-left: 1px;">Próxima consulta em </label>
+<div style="padding: 0px;">
+    <div class="uk-countdown-number uk-countdown-days" style="padding: 0px 5px; font-size: 1rem; font-weight: 900;"></div>
+</div>
+<div class="uk-countdown-separator" style="font-size: 1rem;padding-left: 0px;">:</div>
+<div style="padding: 0px;">
+    <div class="uk-countdown-number uk-countdown-hours" style="padding: 0px 5px; font-size: 1rem; font-weight: 900;"></div>
+</div>
+<div class="uk-countdown-separator" style="font-size: 1rem;padding-left: 0px;">:</div>
+ <div style="padding: 0px;">
+    <div class="uk-countdown-number uk-countdown-minutes" style="padding: 0px 5px; font-size:1rem; font-weight: 900;"></div>	
+</div>
+<div class="uk-countdown-separator" style="font-size: 1rem;padding-left: 0px;">:</div>
+<div style="padding: 0px;">
+    <div class="uk-countdown-number uk-countdown-seconds" style="padding: 0px 5px; font-size: 1rem; font-weight: 900;"></div>
+</div>
+</div>`;
+
 $(function() {    
     moment.locale('pt-br');  
+/*     $(".my-rating").starRating({
+        totalStars: 5,
+        emptyColor: 'lightgray',
+        hoverColor: '#5f338a',
+        activeColor: '#5f338a',
+        strokeColor: "black",
+        ratedColor: "#5f338a",
+        initialRating: 4,
+        strokeWidth: 10,
+        useGradient: false,
+        disableAfterRate: false,
+        callback: function(currentRating, $el){
+            // make a server call here
+            console.log('rated ' + currentRating);
+            console.log('DOM element ', $el);
+        },
+        onHover: function(currentIndex, currentRating, $el){
+            $('.live-rating').text(currentIndex);
+          },
+          onLeave: function(currentIndex, currentRating, $el){
+            $('.live-rating').text(currentRating);
+          }
+    }); */
 });
+
+$(document).on("change", "#select_campus", function(e){
+    const valueSel = this.value;
+    const valueSpl = valueSel.split("-");
+    //console.log(valueSpl);
+    document.getElementById("select_campus").blur();
+    document.getElementById("select_auxilio").disabled = true;
+    document.getElementById("idBtnSend").disabled = true; 
+    /* const dvAux = document.getElementById("divAuxi");
+    dvAux.style.display = "";
+    dvAux.classList.add("uk-animation-slide-bottom"); */  
+    const url = "https://consultaprocessosipac.herokuapp.com/api/v1/campus/"+valueSpl[0]+"/auxilios";
+    //preloader
+    document.getElementById("select_auxilio").innerHTML = '<option value="0" selected disabled>Procurando auxilios...</option>';
+    getFunction(url, renderAuxilios, errorGetAuxilios);
+    e.preventDefault();
+    e.stopImmediatePropagation();
+});
+
+  $(document).on("change", "#select_auxilio", function(e){
+    
+    const btnSend = document.getElementById("idBtnSend");
+    btnSend.disabled = false; 
+    btnSend.style.background = "#5f338a";
+    e.preventDefault();
+    e.stopImmediatePropagation();
+})  
+
+function renderAuxilios(data){
+    //console.log(data);
+    var dataParse = JSON.parse(data);
+    //console.log(dataParse);
+    var dt = dataParse.response.body;
+    var html = "";
+    document.getElementById("select_auxilio").disabled = false;
+    html += '<option value="0" selected disabled>Selecione o auxílio *</option>';
+    for(var i = 0; i < dt.length; i++){
+        html += '<option value="'+dt[i].tipo_auxilio+'">'+dt[i].nome_visualizacao+'</option>';
+    }
+    document.getElementById("select_auxilio").innerHTML = html.toString();
+}
+
+function errorGetAuxilios(_status){
+    //console.log(_status);
+    document.getElementById("idSpinnSend").style.display = "none";
+    document.getElementById("idBtnSend").style.display = "";
+    UIkit.notification({ message: 'Oops! Aconteceu alguma coisa na busca dos auxílios :(  ' + _status, status: 'danger' });
+    return;
+}
+
 
 $(document).on("click", ".send", function(e){
     //console.log("Click send");
@@ -52,7 +156,8 @@ $(document).on("click", ".send", function(e){
     document.getElementById("idBtnSend").style.display = "none";
     var auxilio = document.getElementById("select_auxilio");
     var campus = document.getElementById("select_campus");
-
+    const aux = auxilio.value;
+    const camp = campus.value.split("-");
     if(auxilio.value == 0 || auxilio.value == null || typeof auxilio.value == "undefined"){
         auxilio.classList.add("uk-form-danger");
         setTimeout(function(){
@@ -70,20 +175,38 @@ $(document).on("click", ".send", function(e){
     }else{     
         document.getElementById("renderHtmlProcess").innerHTML = loadSpinn;
         //renderProcess(_dt)
-        getFunction(auxilio.value, campus.value, renderProcess, errorGetProcess);
+        var urlProcess = "https://consultaprocessosipac.herokuapp.com/api/v1/processos?auxilio="+aux+"&campus="+camp[1];
+        //console.log(urlProcess);
+        openLoader();
+        getFunction(urlProcess, renderProcess, errorGetProcess);
     }
 
     e.preventDefault();
     e.stopImmediatePropagation();
 })
 
-function getFunction(auxilio, campus, cbSuccess, cbError) {
+function getFunction(urlProcess, cbSuccess, cbError) {
    // console.log("Chamou func");
+/*    $.ajax({
+    method: "GET",
+    url: url,
+    data: null,
+    dataType: 'json',
+    contentType: "application/json",
+    success: function(body){
+        console.log(body);
+    }, error: function(status){
+        console.log(status)
+    }
+    
+  }); */
+
     var xmlHttp = new XMLHttpRequest();
-    var theUrl = "https://consultaprocessosipac.herokuapp.com/api/v1/processos?auxilio="+auxilio+"&campus="+campus;
+    var theUrl = urlProcess;
 
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            //console.log(xmlHttp.responseText);
             cbSuccess(xmlHttp.responseText);
         } else if (xmlHttp.status == 500) {
             cbError(xmlHttp.status);
@@ -92,11 +215,11 @@ function getFunction(auxilio, campus, cbSuccess, cbError) {
         }
 
     }
-    xmlHttp.open("GET", theUrl, false); // true for asynchronous 
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     //xmlHttp.timeout = 15000;
     xmlHttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xmlHttp.getResponseHeader('Content-Type');
-    xmlHttp.onprogress = updateProgress;
+    //xmlHttp.onprogress = updateProgress;
     xmlHttp.send(null);
 
 
@@ -118,34 +241,49 @@ function updateProgress(evt) {
     }
 }
 
+
 function renderProcess(res) {
+    closeLoader();
     document.getElementById("idSpinnSend").style.display = "none";
     document.getElementById("idBtnSend").style.display = "";
     moment.locale('pt-br'); 
     var resPars = JSON.parse(res);
-    var dt = resPars.data;
+    //console.log(resPars);
+    var dt = resPars.response.body;
+    console.log(dt);
     document.getElementById("renderHtmlProcess").innerHTML = "";
-    var date_hour_now = 'Última pesquisa em: <span id="rendBadgHour">'+moment().format('L') +' - '+ moment().format('LTS')+'</span>';
+    var typeAux = dt.tipo_auxilio.split("_").join(' ');
+    var date_hour_now = '<span style="font-size:13px;">Você consultou pela última vez em: <span id="rendBadgHour">'+moment().format('L') +' - '+ moment().format('LTS')+'</span> o  <b>'+typeAux.toUpperCase()+'</b> do campus <b>'+dt.campus+'</b></span>';
     document.getElementById("lastSearch").innerHTML = date_hour_now;
+    localStorage.setItem('last_search', date_hour_now);
     var html = "";
     document.getElementById("goToDown").click(); 
-    if (dt == "" || typeof dt == "undefined" || dt == null || dt == "Algo deu errado. 'NoneType' object is not iterable") {
+    if (dt == "" || typeof dt == "undefined" || dt == null ) {
         var rp = Math.floor((Math.random() * 4) + 1);
         html += "<div class='classNoFound' style='text-align:center; margin-top:25px;'><img src='images/gato"+rp+".png' style='width: 140px; height: 140px;'/><h2 style='margin-top: 0px; margin-bottom: 0px;'>Oops!</h2><p style='font-size:1.5rem;margin-top: 0px;'>O processo requisitado não foi encontrado, tente novamente mais tarde.</p></div>";
         //errorGetProcess("Nenhum processo encontrado, tente novamente mais tarde.");
-    } else {                   
+    } else { 
+        const t1 = moment(dt.proxima_atualizacao_em).format();
+        //const t2 = moment(t1).add(1, 'hour');   
+        //console.log(t1);
+        const resultDateFormat = (t1);
+        //UIkit.countdown("#countRef", {date: resultDateFormat});
+        //document.getElementById("countRef").style.display = "";
+        const respCount = templateCountDown.replace("{0}", resultDateFormat);	                  
         html += '<article style="margin-top: 20px;" class="page">';
         html += '<h1 class="uk-heading-bullet" style="color: #5f338a; margin-bottom:10px"><span>Resultado</span></h1>';
-        html += '<h4 style="margin-top:0px;margin-bottom:10px;">Atualizado em '+ moment(dt.atualizado_em).format('LLL') +'</h4>';
+        html += '<h4 style="margin-top:5px;margin-bottom:0px;">Processo referente a <strong>'+dt.mes_referente+'</strong></h4>';
+        html += '<h4 style="margin-top:0px;margin-bottom:0px;">Última consulta aos processos do SIPAC em <strong>'+ moment(dt.atualizado_em).format('LLL') +'</strong>. </h4>';
+        html += '<h5 style="margin-top:0px;margin-bottom:10px;">'+respCount+'</h5>';
         html += '<ul class="timeline">';
        // for(var i = 0; i < dt.length; i++){ 
-           var status = "";
-           //console.log(dt);          			
+
+            var status = "";	
             if(dt.status_terminado == true){
                 status = "<span style='color:green'><strong>FINALIZADO</strong></span>";   
                 html += '<li class="timeline-milestone is-completed timeline-start">'; 
                 var title = dt.tipo_auxilio.split("_").join(' ');
-                html += '<div class="timeline-action">';
+                html += '<div class="timeline-action" style="border-top: 3px solid #5f338a;">';
                 html += '<h2 class="title"><b>' + title.toUpperCase() + '</b></span></h2>';
                 html += '<span><b style="font-size: 16px;">Campus: </b> ' + dt.campus  + '</span>';
                 html += '<span class="date"><b>Status:</b> ' + status  + '</span>';
@@ -164,7 +302,7 @@ function renderProcess(res) {
                 status = "<span style='color:#1e87f0'><strong>EM ANDAMENTO</strong></span>"
                 html += '<li class="timeline-milestone is-completed timeline-start">'; 
                 var title = dt.tipo_auxilio.split("_").join(' ');
-                html += '<div class="timeline-action">';
+                html += '<div class="timeline-action" style="border-top: 3px solid #5f338a;">';
                 html += '<h2 class="title"><b>' + title.toUpperCase() + '</b></span></h2>';
                 html += '<span><b style="font-size: 16px;">Campus: </b> ' + dt.campus  + '</span>';
                 html += '<span class="date"><b>Status:</b> ' + status  + '</span>';
@@ -179,7 +317,7 @@ function renderProcess(res) {
                 html += '</div>';
                 html += '</div>';
                 html += '</li>'; 
-                console.log(dt.unidade_destino);
+                //console.log(dt.unidade_destino);
                 for(var i = 0; i < obj_default.length; i++){
                     //console.log(obj_default[i].unidade_destino);                   
                     if(dt.unidade_destino.trim() == obj_default[i].unidade_destino.trim()){
@@ -193,7 +331,7 @@ function renderProcess(res) {
                                 stats = "<span style='color:#5858588f'><strong>AGUARDANDO</strong></span>";   
                                 html += '<li class="timeline-milestone is-future">'; 
                             }
-                            html += '<div class="timeline-action" style="background: #f3f3f3ab; -webkit-box-shadow: 0px 0px 0px 0px #fff; box-shadow: 0px 0px 0px 0px #fff;">';
+                            html += '<div class="timeline-action" style="border-top: 3px solid #b5b5b5; background: #f3f3f3ab; -webkit-box-shadow: 0px 0px 0px 0px #fff; box-shadow: 0px 0px 0px 0px #fff;">';
                             html += '<h2 class="title"><b>' + title.toUpperCase() + '</b></span></h2>';
                             html += '<span><b style="font-size: 16px;">Campus: </b> ' + dt.campus  + '</span>';
                             html += '<span class="date"><b>Status:</b> ' + stats  + '</span>';
@@ -243,6 +381,7 @@ function renderProcess(res) {
 }
 
 function errorGetProcess(_status) {
+    closeLoader();
     document.getElementById("idSpinnSend").style.display = "none";
     document.getElementById("idBtnSend").style.display = "";
     UIkit.notification({ message: 'Oops: ' + _status, status: 'danger' });
@@ -367,3 +506,13 @@ $(document).on("click", ".speakMic", function(e){
       }
     };
 });
+
+function openLoader(){
+    const loader = document.getElementById("id_load");
+    loader.classList.add("is-active");
+}
+
+function closeLoader(){
+    const loader = document.getElementById("id_load");
+    loader.classList.remove("is-active");
+}
